6f549ebf15dd47dc8cfa376c1230705d8dae449e
dawidurbanski <hi@dawidurbanski.com>
2025-11-27 00:03:15 +0100

    feat(codegen): add schema inference utilities and tests

---
+++ packages/codegen/src/inference/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Schema inference utilities
+ *
+ * Provides functions to infer ContentTypeDefinition schemas from various sources:
+ * - UDL Node Store (runtime data)
+ * - GraphQL introspection (external APIs)
+ * - REST API responses (JSON samples)
+ */
+
+export {
+  inferSchemaFromStore,
+  inferFieldType,
+  inferFieldDefinition,
+  mergeFieldDefinitions,
+  type InferFromStoreOptions,
+  type NodeStoreLike,
+} from './from-store.js';
