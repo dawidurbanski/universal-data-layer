740f4abd1a9ad7b629274d509eaf0046193e034b
dawidurbanski <hi@dawidurbanski.com>
2025-11-29 00:35:04 +0100

    feat(contentful): add content types utility functions

---
+++ packages/plugin-source-contentful/src/utils/index.ts
@@ -0,0 +1,11 @@
+export {
+  fetchContentTypes,
+  contentTypeToNodeTypeName,
+  getAssetNodeTypeName,
+  toPascalCase,
+  parseField,
+  parseContentTypeFields,
+  createContentTypeMap,
+  type SimplifiedFieldType,
+  type ParsedField,
+} from './content-types.js';
